21bf35e1a67d7ac27b272875fe9e171630a9a9e8
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-02-02 17:49:38 +0100

    setting values on the cookie

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 module.exports.getUserData = async (req, res) => {
+    req.session.logger = 'loggy log';
     const userData = await Store.find();
     res.json({ userData });
 };
