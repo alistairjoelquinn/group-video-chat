6bd04cc4ae7a443dd72182b754f836978c81b607
alistairjoelquinn <alistairjoelquinn@gmail.com>
2021-02-06 16:34:56 +0100

    logout button allows users to logout whenever

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,13 +1,24 @@
+import { useCallback } from "react";
 import styled from "styled-components";
+
+import axios from './axios';
 
 const ChatStyles = styled.div`
     border: 2px solid green;
 `;
 
 const Chat = () => {
+    const logoutHandler = useCallback(async () => {
+        const { data } = await axios.get('/logout');
+        if (data.loggedOut) {
+            location.replace('/');
+        }
+    }, []);
+
     return (
         <ChatStyles>
             <div>Chat</div>
+            <button onClick={() => logoutHandler()}>Log out</button>
         </ChatStyles>
     );
 };
--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -13,7 +13,7 @@ module.exports.login = (req, res) => {
 
 module.exports.logout = (req, res) => {
     req.session = null;
-    res.redirect('/');
+    res.json({ loggedOut: true });
 };
 
 module.exports.authenticateUser = async (req, res) => {
